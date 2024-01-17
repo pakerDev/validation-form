9e10df7f2f2ef9e3447d6d55783805ff6a90e1f2
Liya <liya.dev@pgpg.la>
2024-01-17 15:16:50 +0800

    feat(formItem): #5 add button function

--- src/pages/FormItem.tsx
+++ src/pages/FormItem.tsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-const FormItem = () => {
+interface IFormItemProps {
+    title: string;
+    limit: number;
+}
+
+const FormItem = (props: IFormItemProps) => {
+    const { title = "hello", limit = 5 } = props;
+
+    const [inp, setInp] = useState([0]);
+    const btnHandler = (l: number) => {
+        if (inp.length < l) {
+            setInp((arr) => [...arr, arr.length]);
+        }
+    };
     return (
         <div>
-            <h1 className='fititle'>title</h1>
+            <h1 className='fititle'>{title}</h1>
             <div>
-                <input className='fiinp' placeholder='ph' type='text' />
+                {inp.map((item, idx) => (
+                    <input key={idx} className='fiinp' placeholder={`${item}`} type='text' disabled={false} />
+                ))}
             </div>
-            <button className='fibtn'>+</button>
+            <button className='fibtn' onClick={() => btnHandler(limit)} disabled={false}>
+                +
+            </button>
         </div>
     );
 };
