9cf29329b90eda02b37a052ab089d09603b08e4b
Liya <liya.dev@pgpg.la>
2024-01-18 17:55:45 +0800

    chore: #2 install plugins

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    presets: [
+        "@babel/preset-env",
+        "@babel/preset-react",
+        // 如果你使用 TypeScript
+        "@babel/preset-typescript",
+    ],
+    plugins: ["macros"],
+};
