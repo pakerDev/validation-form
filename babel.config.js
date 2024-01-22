ad3db880838d7eea44ace45713d1b0caa710077f
Liya <liya.dev@pgpg.la>
2024-01-22 17:26:01 +0800

    style: remove un-use file & plugins

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    presets: [
-        "@babel/preset-env",
-        "@babel/preset-react",
-        // 如果你使用 TypeScript
-        "@babel/preset-typescript",
-    ],
-    plugins: ["macros"],
-};
