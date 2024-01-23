2689f85d04edbca8eb7d73d23e37957bf3c25d34
Liya <liya.dev@pgpg.la>
2024-01-23 17:50:13 +0800

    chore: update data schma

--- src/container/Form.tsx
+++ src/container/Form.tsx
@@ -1,25 +1,41 @@
-import FormItem from "../pages/FormItem.tsx";
+import FormItem from "../components/FormItem.tsx";
+
+export interface IBaseInfoObj {
+    createTime: number;
+    content: string;
+}
+
+export interface IFormItemProps {
+    label: string;
+    limit: number;
+    info: IBaseInfoObj[];
+}
+
+export const data: IFormItemProps[] = [
+    {
+        label: "Title",
+        limit: 1,
+        info: [],
+    },
+    {
+        label: "SubTitle",
+        limit: 3,
+        info: [],
+    },
+    {
+        label: "Description",
+        limit: 5,
+        info: [],
+    },
+];
 
 const Form = () => {
-    const data = [
-        {
-            title: "Title",
-            limit: 1,
-        },
-        {
-            title: "SubTitle",
-            limit: 3,
-        },
-        {
-            title: "Description",
-            limit: 5,
-        },
-    ];
     return (
         <>
             {data.map((item) => (
-                <FormItem key={item.title} title={item.title} limit={item.limit} />
+                <FormItem key={item.label} label={item.label} limit={item.limit} info={item.info} />
             ))}
+            <button type='submit'>submit</button>
         </>
     );
 };
