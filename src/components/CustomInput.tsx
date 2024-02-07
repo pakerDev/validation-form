ce781cc68f0902eb8c8e14cacb745c4deb648a01
Liya <liya.dev@pgpg.la>
2024-02-07 15:00:19 +0800

    feat(customInput): add props error

--- src/components/CustomInput.tsx
+++ src/components/CustomInput.tsx
@@ -1,4 +1,14 @@
-import { Input, InputLabel, FormControl, FormHelperText, TextField, TextFieldPropsSizeOverrides } from "@mui/material";
+import { Checkbox } from "@mui/material";
+import {
+    Input,
+    InputLabel,
+    FormControl,
+    FormHelperText,
+    TextField,
+    TextFieldPropsSizeOverrides,
+    TextFieldProps,
+} from "@mui/material";
+import { useState } from "react";
 
 interface ICustomInput {
     className?: string;
@@ -6,16 +16,48 @@ interface ICustomInput {
     inputLabel: string;
     helperText?: string;
     required?: boolean;
+    error?: boolean;
     fullWidth?: boolean;
     value?: unknown;
     isSmall?: boolean;
+    isShowCheckBox?: boolean;
+    isChecked?: boolean;
+    maxLength?: number;
     onChange?: React.ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>;
+    onBlur?: React.FocusEventHandler<HTMLInputElement | HTMLTextAreaElement>;
+    onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
 const CustomInput = (prop: ICustomInput) => {
-    const { className, id, inputLabel, helperText, required = false, value, fullWidth, onChange } = { ...prop };
+    const {
+        className,
+        id,
+        inputLabel,
+        helperText,
+        required = false,
+        error = false,
+        fullWidth = false,
+        value,
+        maxLength,
+        isShowCheckBox = false,
+        isChecked = true,
+        onChange,
+        onBlur,
+        onClick,
+    } = { ...prop };
+
+    const [count, setCount] = useState(0);
+
+    const handleChange: React.ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement> = (e) => {
+        if (!!maxLength && e.target.value.length >= maxLength) {
+            e.target.value = e.target.value.slice(0, maxLength);
+        }
+        onChange && onChange(e);
+        setCount(e.target.value.length);
+    };
     return (
         <div className='row customInputRow'>
+            {isShowCheckBox && <Checkbox id={id} onClick={onClick} checked={isChecked} />}
             <FormControl>
                 <TextField
                     label={inputLabel}
@@ -23,12 +65,16 @@ const CustomInput = (prop: ICustomInput) => {
                     required={required}
                     id={id}
                     fullWidth={fullWidth}
-                    onChange={onChange}
+                    onChange={handleChange}
                     size='small'
                     type='search'
+                    onBlur={onBlur}
+                    error={error}
+                    multiline
                 />
                 <FormHelperText>{helperText}</FormHelperText>
             </FormControl>
+            {!!maxLength && <span className='customInputCount'>{`${count}/${maxLength}`}</span>}
         </div>
     );
 };
