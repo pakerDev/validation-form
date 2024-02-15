ea7d33b85da4410bf7ae267eddaca4e3287d5cfe
Liya <liya.dev@pgpg.la>
2024-02-16 01:57:27 +0800

    feat: #56 add search bar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,23 @@
+import CustomSearch from "../components/CustomSearch";
 import CustomDropDown from "../components/CustomDropDown";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { mainData } from "../constant/configs";
 import CustomPreview from "../components/CustomPreview";
 import CustomPreviewPro from "../components/CustomPreviewPro";
-import { IMainData } from "../constant/types";
+import { IMainData, ISearchInfo, allTagsType } from "../constant/types";
 import Studio from "../components/CustomStudioContainer";
-import { fetchData } from "../constant/main";
+import VideoPage from "./VideoPage";
+import CustomNav from "../components/CustomNav";
+import CustomEditorModal from "../container/CustomEditorModal";
+import WarningModal from "../components/WarningModal";
 
 const Home = () => {
     !localStorage.getItem("mainData") && localStorage.setItem("mainData", JSON.stringify(mainData));
-    const savedDataJson = fetchData();
+    const [savedDataJson, setSavedDataJson] = useState(JSON.parse(localStorage.getItem("mainData") ?? ""));
     const [isCardMode, setIsCardMode] = useState(true);
     const [isHomePage, setIsHomePage] = useState(true);
+    const [category, setCategory] = useState<allTagsType>("all");
+    const [searchInfo, setSearchInfo] = useState<ISearchInfo>({ by: "title", keyWord: "" });
 
     const dropDownHandler = (mode: string) => {
         if (mode === "cardMode") {
@@ -22,13 +28,81 @@ const Home = () => {
         }
     };
 
+    const navClickHandler = (cate: allTagsType) => {
+        setCategory(cate);
+    };
+
+    const searchBarHandler = ({ by, keyWord }: ISearchInfo) => {
+        setSearchInfo({ by: by, keyWord: keyWord });
+    };
+
+    useEffect(() => {
+        const allUploadedData = JSON.parse(localStorage.getItem("mainData") ?? "").filter(
+            (i: IMainData) => i.isUploaded === true
+        );
+
+        let filteredData;
+        if (searchInfo.by === "desc") {
+            //todo 模糊搜尋
+            if (searchInfo.keyWord === "") {
+                filteredData = [...allUploadedData];
+            } else {
+                filteredData = allUploadedData.filter((item: IMainData) =>
+                    item.desc.some((descItem) => descItem.includes(searchInfo.keyWord))
+                );
+            }
+        } else {
+            filteredData = allUploadedData.filter((item: IMainData) =>
+                item[searchInfo.by].toString().includes(searchInfo.keyWord)
+            );
+        }
+
+        if (category === "all") {
+            setSavedDataJson(
+                !!searchInfo.keyWord
+                    ? filteredData.filter((item: IMainData) =>
+                          item[searchInfo.by].toString().includes(searchInfo.keyWord)
+                      )
+                    : filteredData
+            );
+        } else if (category === "bookmark") {
+            const allLickedData = filteredData.filter((i: IMainData) => i.isLiked === true);
+            setSavedDataJson(
+                !!searchInfo.keyWord
+                    ? allLickedData.filter((item: IMainData) =>
+                          item[searchInfo.by].toString().includes(searchInfo.keyWord)
+                      )
+                    : allLickedData
+            );
+        } else {
+            const theTagData = filteredData.filter((i: IMainData) => i.tag.includes(category));
+            setSavedDataJson(
+                !!searchInfo.keyWord
+                    ? theTagData.filter((item: IMainData) =>
+                          item[searchInfo.by].toString().includes(searchInfo.keyWord)
+                      )
+                    : theTagData
+            );
+        }
+    }, [category, searchInfo]);
+
     return (
         <>
             {isHomePage ? (
                 <>
                     <div className='row homeHead'>
                         <span></span>
+
+                        <CustomSearch
+                            homeSearch={({ by, keyWord }: ISearchInfo) => searchBarHandler({ by, keyWord })}
+                        />
                         <CustomDropDown homeViewMode={(mode: string) => dropDownHandler(mode)} />
+                    </div>
+                    <div className='row homeNav'>
+                        <CustomNav
+                            homeNavStatus={(category: allTagsType) => navClickHandler(category)}
+                            status={category}
+                        />
                     </div>
                 </>
             ) : (
@@ -49,7 +123,9 @@ const Home = () => {
                         <Studio />
                     );
                 })}
+                {/* <CustomEditorModal type='' data={mainData[0]} /> */}
             </div>
+            {/* <WarningModal handleConfirmClick={() => {}} closePopupHandler={() => {}} /> */}
         </>
     );
 };
